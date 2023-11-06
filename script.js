e7203cb55d3a168e71b67de870f859d94aac5e6c
Manwe <mathieu.jourdan.zola@gmail.com>
2023-11-06 16:58:27 +0100

    Chapitre : Conditions

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let score = 0;
+let motUtilisateur = prompt("Entrez un mot :" + listeMots[0])
+
+if(motUtilisateur === listeMots[0])
+{
+    console.log("Bravo vous avez gagné !")
+    score++
+    console.log("Score : " + score)
+} else {
+    console.log("Dommage ce n'est pas le bon mot")
+}
+
+motUtilisateur = prompt("Entrez un mot :" + listeMots[1])
+
+if(motUtilisateur === listeMots[1])
+{
+    console.log("Bravo vous avez gagné !")
+    score++
+    console.log("Score : " + score)
+} else {
+    console.log("Dommage ce n'est pas le bon mot")
+}
+
+motUtilisateur = prompt("Entrez un mot :" + listeMots[2])
+
+if(motUtilisateur === listeMots[2])
+{
+    console.log("Bravo vous avez gagné !")
+    score++
+    console.log("Score : " + score)
+} else {
+    console.log("Dommage ce n'est pas le bon mot")
+}
+
+console.log("Score final : " + score)
